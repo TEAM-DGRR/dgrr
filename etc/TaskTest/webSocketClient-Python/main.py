d7fe7cd4b974bb752744598779c9548407cbfc02
itsdangerous <danger121@naver.com>
2023-07-26 16:21:48 +0900

    feat: 연결 끊어지면 다시 요청하는 기능 추가 - while문 추가

--- etc/TaskTest/webSocketClient-Python/main.py
+++ etc/TaskTest/webSocketClient-Python/main.py
@@ -29,30 +29,31 @@ async def startup_event():
 
 
 async def client():
-    try:
-        uri = "ws://localhost:8080/ws"  # 자바 웹소켓 서버의 주소
+    while True :
+        try:
+            uri = "ws://localhost:8080/ws"  # 자바 웹소켓 서버의 주소
 
-        # Spring boot Security 사용자 이름
-        username = "1234"
+            # Spring boot Security 사용자 이름
+            username = "1234"
 
-        # Spring Boot Security 비밀번호
-        password = "1234"
-        encoded_credentials = base64.b64encode(
-            f"{username}:{password}".encode()
-        ).decode()
+            # Spring Boot Security 비밀번호
+            password = "1234"
+            encoded_credentials = base64.b64encode(
+                f"{username}:{password}".encode()
+            ).decode()
 
-        # 웹소켓 서버에 연결
-        async with websockets.connect(
-            uri, extra_headers={"Authorization": f"Basic {encoded_credentials}"}
-        ) as websocket:
-            await connect_to_server(websocket)
-            await receive_and_process_message(
-                websocket, face_cascade, emotion_model, emotions
-            )
-    except Exception as e:
-        print(f"웹소켓 클라이언트에서 예외 발생: {e}")
-        print(f"3초 후에 재연결을 시도합니다.)")
-        await asyncio.sleep(3)  # 3초 동안 대기
+            # 웹소켓 서버에 연결
+            async with websockets.connect(
+                uri, extra_headers={"Authorization": f"Basic {encoded_credentials}"}
+            ) as websocket:
+                await connect_to_server(websocket)
+                await receive_and_process_message(
+                    websocket, face_cascade, emotion_model, emotions
+                )
+        except Exception as e:
+            print(f"웹소켓 클라이언트에서 예외 발생: {e}")
+            print(f"3초 후에 재연결을 시도합니다.)")
+            await asyncio.sleep(3)  # 3초 동안 대기
 
 
 if __name__ == "__main__":

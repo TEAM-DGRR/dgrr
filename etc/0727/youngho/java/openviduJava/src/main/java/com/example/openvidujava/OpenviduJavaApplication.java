c2643ec45fdc2dc00b91b0d463ec0db462594618
전영호 <aplblypt@gmail.com>
2023-07-27 18:01:55 +0900

    docs: openvidu apply on ec2

---
+++ etc/0727/youngho/java/openviduJava/src/main/java/com/example/openvidujava/OpenviduJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.openvidujava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenviduJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenviduJavaApplication.class, args);
+	}
+
+}
---
+++ etc/0727/youngho/java/openviduJava/src/test/java/com/example/openvidujava/OpenviduJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openvidujava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenviduJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

cb913e67329153a5ff4ba9e7cdf6a3f4349b8061
전영호 <aplblypt@gmail.com>
2023-07-31 13:11:38 +0900

    chore: Spring Boot Initialization and Set Openvidu url and password

---
+++ backend/dgrr/src/test/java/live/dgrr/DgrrApplicationTests.java
@@ -0,0 +1,13 @@
+package live.dgrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

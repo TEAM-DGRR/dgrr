74dd95322833bb60d79472b9464c0348be0a99fb
최명하 <lunartown@gmail.com>
2023-08-02 10:19:30 +0900

    remove: etc folder remove

--- frontend/etc/SampleCodes/getToken.ts
+++
@@ -1,7 +0,0 @@
-import { createSession } from "./createSession";
-import { createToken } from "./createToken";
-
-export const getToken = async (mySessionId: string) => {
-  const sessionId = await createSession(mySessionId);
-  return await createToken(sessionId);
-};

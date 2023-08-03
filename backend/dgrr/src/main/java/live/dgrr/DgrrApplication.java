a3187d920799dd1988b447dd80c37112b993f1da
mjpark1329 <mjpark1329@naver.com>
2023-08-03 13:02:52 +0900

    feat: JPA Auditing 설정 & 객체 변경 application.yml -> JPA ddl-auto 설정 update에서 create로 변경 DgrrApplication.java -> JPA autiding enable 추가 Member.java -> nickname 속성 추가 BaseEntity.java -> JPA auditing을 통해 createdAd, modifiedAt을 모든 도메인에 추가하기 위한 entity

--- backend/dgrr/src/main/java/live/dgrr/DgrrApplication.java
+++ backend/dgrr/src/main/java/live/dgrr/DgrrApplication.java
@@ -2,8 +2,9 @@ package live.dgrr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DgrrApplication {
 
--- backend/dgrr/src/main/java/live/dgrr/domain/member/Member.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/member/Member.java
@@ -1,31 +1,24 @@
 package live.dgrr.domain.member;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import live.dgrr.global.domain.BaseEntity;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
+import lombok.Setter;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
 
 @Entity
-@Table(name = "member")
-@Data
-@Builder
 @NoArgsConstructor
-@AllArgsConstructor
-public class Member {
+@Setter
+public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "member_id")
     private Long memberId;
 
-    @Column(name = "kakao_id")
     private String kakaoId;
 
-    @Column(name = "profile_image")
+    private String nickname;
+
     private String profileImage;
 
     private String description;
@@ -33,21 +26,8 @@ public class Member {
     // refreshToken은 객체 따로 만들기
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "member_role")
     private RoleType memberRole;
 
     private String status;
 
-    @Column(name = "created_at")
-    @CreationTimestamp
-    private Timestamp createdAt;
-
-    @Column(name = "modified_at")
-    private Timestamp modifiedAt;
-
-    @Builder
-    public Member(String kakaoId, String memberRole) {
-        this.kakaoId = kakaoId;
-        this.memberRole = RoleType.valueOf(memberRole);
-    }
 }

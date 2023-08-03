4f3d9d98f284b4293fd0dbd12bb4ea66ef7af6f8
mjpark1329 <mjpark1329@naver.com>
2023-08-03 13:27:18 +0900

    feat: Member 객체 수정(setter 삭제)

--- backend/dgrr/src/main/java/live/dgrr/domain/member/Member.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/member/Member.java
@@ -1,14 +1,14 @@
 package live.dgrr.domain.member;
 
 import live.dgrr.global.domain.BaseEntity;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
-@Setter
+@Getter
 public class Member extends BaseEntity {
 
     @Id
--- backend/dgrr/src/main/java/live/dgrr/domain/member/MemberController.java
+++ backend/dgrr/src/main/java/live/dgrr/domain/member/MemberController.java
@@ -1,6 +1,5 @@
 package live.dgrr.domain.member;
 
-import live.dgrr.domain.oauth.OauthToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;

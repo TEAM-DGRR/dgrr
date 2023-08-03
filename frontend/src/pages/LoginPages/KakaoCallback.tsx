e5ecb53a042b81867c7b6091fb1d06b33360641c
hyuna <kook980316@gmail.com>
2023-08-03 12:41:56 +0900

    feat: 백에서 유저 정보 받아와서 처리 key가 있으면 메인으로 없으면 회원가입으로 보내기

--- frontend/src/pages/LoginPages/KakaoCallback.tsx
+++ frontend/src/pages/LoginPages/KakaoCallback.tsx
@@ -1,25 +1,30 @@
 import { useEffect } from "react";
 import axios from "axios"
+import { useNavigate } from "react-router-dom";
 
 export const KakaoCallback = () => {
+    const navigate = useNavigate();
+
     useEffect(() => {
         // 인가 코드 받기
         const code = new URL(window.location.href).searchParams.get("code");
 
         // 인가 코드 보내기
-        axios.post(
-            `/kakao/kakaoCallback`,
-            { code : code }
+        axios.get(
+            `http://localhost:8080/member/kakaoCallback?code=${code}`,
         )
         .then((res: any) => {
-            // 유저 정보가 있으면 메인으로 보내기
-
             // 없다면 회원가입 화면으로 보내기
+            if (res.data.key === "signUp") {
+                navigate('/signup')
+            }
+            // 유저 정보가 있으면 메인으로 보내기
+            navigate('/main')
         })
         .catch((Error: any) => {
             console.log(Error)
         })
-    }, [])
+    })
     
     return(
         <>

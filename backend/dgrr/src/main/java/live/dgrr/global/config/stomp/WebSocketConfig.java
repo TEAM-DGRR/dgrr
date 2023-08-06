d209b5c0420bd4b6ecac1dc22179f04e22a5c25a
전영호 <aplblypt@gmail.com>
2023-08-06 17:41:06 +0900

    feat: WebSocketConfig 에 withSockJs 추가.

--- backend/dgrr/src/main/java/live/dgrr/global/config/stomp/WebSocketConfig.java
+++ backend/dgrr/src/main/java/live/dgrr/global/config/stomp/WebSocketConfig.java
@@ -18,6 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("*", "http://localhost:3000")
-                .setHandshakeHandler(new CustomHandshakeHandler());
+                .setHandshakeHandler(new CustomHandshakeHandler())
+                .withSockJS();
     }
 }

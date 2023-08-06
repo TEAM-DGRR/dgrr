76dc65b5e8d87d839dadd6c12b0cef56d1db3be8
전영호 <aplblypt@gmail.com>
2023-08-06 20:36:01 +0900

    remove: WebSocketConfig 에 withSockJs 다시 제거.

--- backend/dgrr/src/main/java/live/dgrr/global/config/stomp/WebSocketConfig.java
+++ backend/dgrr/src/main/java/live/dgrr/global/config/stomp/WebSocketConfig.java
@@ -18,7 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("*", "http://localhost:3000")
-                .setHandshakeHandler(new CustomHandshakeHandler())
-                .withSockJS();
+                .setHandshakeHandler(new CustomHandshakeHandler());
     }
 }

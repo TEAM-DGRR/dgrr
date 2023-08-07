96f51a6dc534688021d13596047fec1d92298e35
hyuna <kook980316@gmail.com>
2023-08-07 16:50:02 +0900

    feat: profile page route 추가

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -5,6 +5,7 @@ import { KakaoLogin } from "pages/LoginPages/KakaoLogin";
 import { KakaoCallback } from "pages/LoginPages/KakaoCallback";
 import { SignUp } from "pages/LoginPages/SignUp";
 import { Main } from "pages/MainPages/Main";
+import { MyProfile } from "pages/ProfilePages/MyProfile";
 
 export const App = () => {
   return (
@@ -17,6 +18,7 @@ export const App = () => {
 
         <Route path="/main" element={<Main />} />
         <Route path="/game" element={<Game />} />
+        <Route path="/myprofile" element={<MyProfile />} />
       </Routes>
     </div>
   );

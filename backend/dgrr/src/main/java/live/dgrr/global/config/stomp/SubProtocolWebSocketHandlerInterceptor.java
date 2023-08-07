d29674d4e3cb2f820a4efc90494e2409b557baac
itsdangerous <danger121@naver.com>
2023-08-07 10:56:51 +0900

    feat: 버퍼 사이즈 설정하기 위해 Config에 등록할 인터셉터

--- backend/dgrr/src/main/java/live/dgrr/global/config/stomp/SubProtocolWebSocketHandlerInterceptor.java
+++ backend/dgrr/src/main/java/live/dgrr/global/config/stomp/SubProtocolWebSocketHandlerInterceptor.java
@@ -1,2 +1,21 @@
-package live.dgrr.global.config.stomp;public class SubProtocolWebSocketHandlerInterceptor {
+// 64kb 이상의 데이터를 보내기 위한 Stomp 설정
+// 이 곳에는 실제 사이즈 제한에 대한 내용을 작성
+
+package live.dgrr.global.config.stomp;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.support.DelegatingIntroductionInterceptor;
+import org.springframework.web.socket.WebSocketSession;
+
+public class SubProtocolWebSocketHandlerInterceptor extends DelegatingIntroductionInterceptor {
+
+    @Override
+    protected Object doProceed(MethodInvocation mi) throws Throwable {
+        if(mi.getMethod().getName().equals("afterConnectionEstablished")) {
+            WebSocketSession session = (WebSocketSession) mi.getArguments()[0];
+            session.setTextMessageSizeLimit(60 * 1024 * 1024);
+            session.setBinaryMessageSizeLimit(60 * 1024 * 1024);
+        }
+        return super.doProceed(mi);
+    }
 }

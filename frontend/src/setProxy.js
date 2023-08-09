4818f7373d8d35555aad11cb42c7648a70faab16
mjpark1329 <mjpark1329@naver.com>
2023-08-09 17:38:33 +0900

    fix: 코드리뷰 반영

--- frontend/src/setProxy.js
+++ frontend/src/setProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api/v1", {
-      target: "http://localhost:8080",
+      target: "${process.env.REACT_APP_API_URL}",
       changeOrigin: true,
     })
   );

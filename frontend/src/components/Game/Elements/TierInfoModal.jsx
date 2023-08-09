30f0b6924585123090228e257ca59cfb0e7ccdfa
이수연 <sooyeon990828@naver.com>
2023-08-09 17:02:28 +0900

    design: mypage 디자인 css 작성, 티어 정보 모달 생성

---
+++ frontend/src/components/Game/Elements/TierInfoModal.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import 'assets/scss/TierInfoModal.scss';
+import tierBronze from 'assets/images/tier_bronze.png';
+import tierSilver from 'assets/images/tier_silver.png';
+import tierGold from 'assets/images/tier_gold.png';
+
+const Modal = ({ isOpen, closeModal }) => {
+	const handleOverlayClick = (e) => {
+		if (e.target === e.currentTarget) {
+			closeModal();
+		}
+	};
+
+	if (!isOpen) return null;
+
+	//battleDetailList
+	const tierList = [
+		{
+			tierName: 'bronze',
+			tierImage: tierBronze,
+			lowRating: 0,
+		},
+		{
+			tierName: 'silver',
+			tierImage: tierSilver,
+			lowRating: 1600,
+		},
+		{
+			tierName: 'gold',
+			tierImage: tierGold,
+			lowRating: 1800,
+		},
+	];
+
+	return (
+		<div className='modal' onClick={handleOverlayClick}>
+			<div className='modal-content'>
+				<ul className='list-tier'>
+					{tierList.map((item, index) => (
+						<li key={index} className='tier-item'>
+							<div className='tier-info'>
+								<img className='tier-image' src={item.tierImage} alt='티어 이미지' />
+								<div className='tier-descption'>
+									<span className='tier-name'>{item.tierName}</span>
+									<span className='tier-lowRating'>{item.lowRating}~</span>
+								</div>
+							</div>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default Modal;

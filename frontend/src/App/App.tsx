aa72940b818d8003389a87fa0174b7a5f902c169
이수연 <sooyeon990828@naver.com>
2023-08-10 17:47:30 +0900

    design: 게임 결과 화면 css

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,33 +1,35 @@
-import { useEffect } from "react";
-import "assets/scss/App.scss";
-import { Route, Routes } from "react-router-dom";
-import { Game } from "pages/GamePages/Game";
-import { KakaoLogin } from "pages/LoginPages/KakaoLogin";
-import { KakaoCallback } from "pages/LoginPages/KakaoCallback";
-import { SignUp } from "pages/LoginPages/SignUp";
-import { Main } from "pages/MainPages/Main";
+import { useEffect } from 'react';
+import 'assets/scss/App.scss';
+import { Route, Routes } from 'react-router-dom';
+import { Game } from 'pages/GamePages/Game';
+import { KakaoLogin } from 'pages/LoginPages/KakaoLogin';
+import { KakaoCallback } from 'pages/LoginPages/KakaoCallback';
+import { SignUp } from 'pages/LoginPages/SignUp';
+import { Main } from 'pages/MainPages/Main';
+import { GameResult } from 'pages/GamePages/GameResult';
 
 export const App = () => {
-  // 모바일 뷰포트 계산
-  function setScreenSize() {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  }
-  useEffect(() => {
-    setScreenSize();
-  });
+	// 모바일 뷰포트 계산
+	function setScreenSize() {
+		const vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+	}
+	useEffect(() => {
+		setScreenSize();
+	});
 
-  return (
-    <div className="App">
-      <Routes>
-        {/* 로그인 관련 */}
-        <Route path="/" element={<KakaoLogin />} />
-        <Route path="/KakaoCallback" element={<KakaoCallback />} />
-        <Route path="/signup" element={<SignUp />} />
+	return (
+		<div className='App'>
+			<Routes>
+				{/* 로그인 관련 */}
+				<Route path='/' element={<KakaoLogin />} />
+				<Route path='/KakaoCallback' element={<KakaoCallback />} />
+				<Route path='/signup' element={<SignUp />} />
 
-        <Route path="/main" element={<Main />} />
-        <Route path="/game/*" element={<Game />} />
-      </Routes>
-    </div>
-  );
+				<Route path='/main' element={<Main />} />
+				<Route path='/game/*' element={<Game />} />
+				<Route path='/result' element={<GameResult />}></Route>
+			</Routes>
+		</div>
+	);
 };

a8d2f63fa084a1f9279678304e30f166f17e6e8b
itsdangerous <danger121@naver.com>
2023-08-10 20:47:03 +0900

    feat: 게임 로딩 화면 ui 구현 - 라우팅 설정 필요

--- frontend/src/pages/GamePages/GameLoading.tsx
+++ frontend/src/pages/GamePages/GameLoading.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import "assets/scss/Loding.scss";
-import LoadingLogo from "assets/images/logo_character.png";
+import { useEffect, useState } from 'react';
 
-const LoadingMessage = "게임을 찾는 중입니다...";
+import LoadingLogo from "assets/images/logo_character.png";
+import "assets/scss/Loding.scss";
+
+const LoadingMessage = "게임을 찾는 중입니다";
 export const GameLoading = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(prev => prev + 1);
+    }, 1000);
+
+    // Cleanup on unmount
+    return () => clearInterval(interval);
+  }, []);
+  
   return (
     <div className="GameLoadingScreen">
       <div className="RotatingElement">
         <img src={LoadingLogo} alt="a"/>
       </div>
+      <div className="Timer">{seconds}s</div> {/* This is the new timer display */}
       <div className="LoadingText">
         {Array.from(LoadingMessage).map((char, index) => (
           <span key={index}>{char}</span>

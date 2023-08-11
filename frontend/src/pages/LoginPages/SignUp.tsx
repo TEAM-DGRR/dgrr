45d8a78df6822b0793606ed7e91f59c264a0b5cf
hyuna <kook980316@gmail.com>
2023-08-11 10:03:54 +0900

    feat: 닉네임 중복 검사 버튼을 삭제 중복검사 버튼을 삭제하고 회원가입 버튼을 누르면 검사를 진행하도록 변경

--- frontend/src/pages/LoginPages/SignUp.tsx
+++ frontend/src/pages/LoginPages/SignUp.tsx
@@ -34,8 +34,6 @@ export const SignUp = () => {
     fileInputRef.current?.click();
   };
 
-  // 중복검사 버튼 문구
-  const [checkbtn, setCheckbtn] = useState("중복검사");
   // 닉네임 관련 경고 문구
   const [checkstate, setCheckstate] = useState("");
 
@@ -43,8 +41,6 @@ export const SignUp = () => {
   const [nickname, setNickname] = useState("");
   // 상태 메세지 state
   const [description, setDescription] = useState("");
-  // 닉네임 중복 검사 통과 state
-  const [isChecked, setChecked] = useState(false);
   // 경고 문구 visibility state
   const [see, setSee] = useState(false);
 
@@ -52,49 +48,10 @@ export const SignUp = () => {
   const onChangeNickname = (e: ChangeEvent<HTMLInputElement>) => {
     setNickname(e.target.value);
     setSee(false);
-    setChecked(false);
-    setCheckbtn("중복확인");
   };
   // 상태메시지 변경상태 받기
   const onChangeDescription = (e: ChangeEvent<HTMLInputElement>) => {
     setDescription(e.target.value);
-  };
-
-  // 닉네임 중복확인
-  const nicknameCheck = (e: any) => {
-    e.preventDefault();
-    setCheckbtn("중복검사");
-    setSee(false);
-    setChecked(false);
-
-    // 닉네임이 두글자 이상이고 특수기호가 없다면 중복확인
-    const regex = /^[a-zA-Z가-힣0-9]*$/;
-    if (nickname.length >= 2 && regex.test(nickname)) {
-      // 닉네임 보내서 중복인지 확인하기
-      axios
-        .get(
-          `${process.env.REACT_APP_API_URL}/member/nickname-check?nickname=${nickname}`
-        )
-        .then((res: any) => {
-          // 없다면 사용 가능하다고 알려주고 중복검사 state 변경해주기
-          if (res.data.nicknameExists === "false") {
-            setCheckbtn("확인완료");
-            setChecked(true);
-          }
-          // 존재하면 경고 문구 보여주기
-          else {
-            setCheckstate("이미 존재하는 닉네임입니다");
-            setSee(true);
-          }
-        });
-    } else {
-      if (nickname.length < 2) {
-        setCheckstate("닉네임을 2글자 이상 입력해주세요");
-      } else {
-        setCheckstate("한글/영어/숫자만 사용해주세요");
-      }
-      setSee(true);
-    }
   };
 
   useEffect(() => {
@@ -130,37 +87,61 @@ export const SignUp = () => {
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
 
-    // 닉네임 중복검사 통과했으면 회원가입 시켜주기
-    if (isChecked) {
-      axios.post(
-          `${process.env.REACT_APP_API_URL}/member`,
-          {
-            kakaoId: id,
-            nickname: nickname,
-            profileImg: profileImg,
-            description: description,
-          }
-        ).then((res: any) => {
-        console.log(res.data)
-        axios.get(`${process.env.REACT_APP_API_URL}/member/login?kakaoId=${res.data.kakaoId}`)
+    const regex = /^[a-zA-Z가-힣0-9]*$/;
+    // 닉네임 조건을 만족한다면 중복검사
+    if (nickname.length >= 2 && regex.test(nickname)) {
+      axios
+        .get(
+          `${process.env.REACT_APP_API_URL}/member/nickname-check?nickname=${nickname}`
+        )
         .then((res: any) => {
-          console.log("login data: " + JSON.stringify(res.data));
-          localStorage.setItem("token", res.data.token);
-          axios.defaults.headers.common["Authorization"] = `${res.data.token}`;
-          axios.get(`${process.env.REACT_APP_API_URL}/member/kakao-id?kakaoId=${res.data.member.kakaoId}`)
-          .then((res :any) => {
-            console.log(JSON.stringify(res.data))
-          })
+          // 없다면 회원가입 진행
+          if (res.data.nicknameExists === "false") {
+            axios
+              .post(`${process.env.REACT_APP_API_URL}/member`, {
+                kakaoId: id,
+                nickname: nickname,
+                profileImg: profileImg,
+                description: description,
+              })
+              .then((res: any) => {
+                console.log(res.data);
+                axios
+                  .get(
+                    `${process.env.REACT_APP_API_URL}/member/login?kakaoId=${res.data.kakaoId}`
+                  )
+                  .then((res: any) => {
+                    console.log("login data: " + JSON.stringify(res.data));
+                    localStorage.setItem("token", res.data.token);
+                    axios.defaults.headers.common[
+                      "Authorization"
+                    ] = `${res.data.token}`;
+                    axios
+                      .get(
+                        `${process.env.REACT_APP_API_URL}/member/kakao-id?kakaoId=${res.data.member.kakaoId}`
+                      )
+                      .then((res: any) => {
+                        console.log(JSON.stringify(res.data));
+                      });
+                  });
+                navigate("/main");
+              })
+              .catch((err: any) => {
+                console.log(err);
+              });
+          }
+          // 존재하면 경고 문구 보여주기
+          else {
+            setCheckstate("이미 존재하는 닉네임입니다");
+            setSee(true);
+          }
         });
-
-        navigate("/main");
-      })
-      }).catch((err: any) => {
-        console.log(err)
-      })
     } else {
-      // 중복검사 통과못했으면 경고
-      setCheckstate("닉네임을 확인해주세요");
+      if (nickname.length < 2) {
+        setCheckstate("닉네임을 2글자 이상 입력해주세요");
+      } else {
+        setCheckstate("한글/영어/숫자만 사용해주세요");
+      }
       setSee(true);
     }
   };
@@ -217,9 +198,6 @@ export const SignUp = () => {
               onChange={onChangeNickname}
               placeholder={"(필수)닉네임을 입력해주세요"}
             />
-            <button id="nicknamebtn" onClick={nicknameCheck}>
-              {checkbtn}
-            </button>
           </div>
           <div className="check-state">
             <span

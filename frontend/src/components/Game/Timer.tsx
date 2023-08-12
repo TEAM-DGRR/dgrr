45baa6fb42629409ab6f0555df7853a67b999b7c
hyuna <kook980316@gmail.com>
2023-08-12 16:05:58 +0900

    feat: 시간 보여주는 timer 생성

---
+++ frontend/src/components/Game/Timer.tsx
@@ -0,0 +1,37 @@
+import { memo, useEffect, useState } from "react";
+import "assets/scss/GamePlay.scss";
+
+export const Timer = memo(() => {
+    // 30초
+    const playTime = 30 * 1000;
+    // 1초
+    const INTERVAL = 1000;
+    // 남은 시간
+    const [leftTime, setLeftTime] = useState<number>(playTime);
+
+
+    // ms 초로 변환
+    const second = String(Math.floor((leftTime / 1000) % 60)).padStart(2, '0');
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setLeftTime((prevTime) => prevTime - INTERVAL);
+        }, INTERVAL);
+
+        if (leftTime <= 0) {
+            // 0초가 되면 timer 종료
+            clearInterval(timer)
+            console.log('시간 끝')
+        }
+        
+        return () => {
+            clearInterval(timer);
+        };
+    }, [leftTime]);
+
+    return (
+        <div className="timer">
+            0 : {second}
+        </div>
+    );
+});

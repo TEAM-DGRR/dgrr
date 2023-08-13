08cd903bf6d1377a3e03075224f5693ee2e6e1c3
itsdangerous <danger121@naver.com>
2023-08-14 01:20:05 +0900

    fix: round 2가 되어도 round 1로 보내는 오류 해결

--- frontend/src/pages/GamePages/GamePlay.tsx
+++ frontend/src/pages/GamePages/GamePlay.tsx
@@ -1,5 +1,3 @@
-// GamePlay.tsx
-
 import { Client, IMessage } from "@stomp/stompjs";
 import attackIco from "assets/images/match-attack.png";
 import defendIco from "assets/images/match-defense.png";
@@ -32,16 +30,13 @@ export interface ChildMethods {
 }
 
 export const GamePlay = () => {
-  const navigate = useNavigate();
   const { stompClient, isStompConnected, gameConfig } = useGameContext();
   const { gameSessionId, openViduToken, startTime, myInfo, enemyInfo, turn } =
-   
     gameConfig;
 
   // Stomp
   const { DESTINATION_URI, CAPTURE_INTERVAL } = stompConfig;
   const { IMAGE_DATA_URI, IMAGE_RESULT_URI, STATUS_URI, RESULT_URI } =
-   
     DESTINATION_URI;
 
   // 이미지 처리
@@ -59,30 +54,22 @@ export const GamePlay = () => {
   const { PUBLISHER_PROPERTIES } = openViduConfig;
 
   // 게임 상태
-  const [status, setStatus] = useState<string>("ready");
-  const [role, setRole] = useState<string>("");
-  const [round, setRound] = useState<string>("round 1");
-  const [success, setSuccess] = useState<boolean>(false);
-  const [emotion, setEmotion] = useState<string>("");
-  const [probability, setProbability] = useState<string>("");
-  const [message, setMessage] = useState<string>("");
+  const [role, setRole] = useState<string>(""); // 초기 상태
+  const [round, setRound] = useState<string>("round 1"); // 초기 상태 "round 1"
 
-  // 1) 게임 시작 준비
+  // gameConfig의 turn에 따라 role을 설정
   useEffect(() => {
-    const gameStart = () => {
-      console.log("게임을 시작합니다!");
-      if (turn === "first") setRole("attack");
-      else setRole("defense");
-      console.log("round : ", round);
-      console.log("role : ", role);
-    };
+    if (gameConfig.turn === "first") {
+      setRole("attack");
+    } else if (gameConfig.turn === "second") {
+      setRole("defense");
+    }
+  }, [gameConfig]);
 
-    setTimeout(gameStart, timeRemaining(startTime));
-  }, [role, round]);
-
-  //OV, Session Create
+  // GamePlay 렌더링 시 OvenVidu 연결
   useEffect(() => {
     initGame().then(({ OV, session }) => {
+      console.log("THE FIRST OV INItialte");
       setOV(OV);
       setOVSession(session);
       session.on("streamCreated", (event) => {
@@ -104,13 +91,15 @@ export const GamePlay = () => {
   }, []);
 
   useEffect(() => {
-    // 라운드가 변경될 때 한번 실행됨.
     const roundEnd = (gameStatus: IGameStatus) => {
-      const roundStart = () => {
-        if (role === "attack") setRole("defense");
-        if (role === "defense") setRole("attack");
-        setRound("round 2");
-      };
+      const roundStart = () => {};
+
+      if (gameStatus.status === "round changed") {
+        console.log("2라운드를 진행합니다.");
+      } else {
+        // gameStatus.status === "End"
+        console.log("게임 상태 파싱 오류 : 라운드 전환 불가");
+      }
 
       setTimeout(roundStart, timeRemaining(gameStatus.startTime));
     };
@@ -119,28 +108,43 @@ export const GamePlay = () => {
       console.log("게임 종료");
       stompClient?.deactivate();
       OVSession?.disconnect();
-      navigate("game/result");
+      navigate("/game/result");
     };
 
     // Stomp 엔드포인트 구독
     if (stompClient !== undefined) {
+      // 1. 이미지 결과 받기
       stompClient.subscribe(IMAGE_RESULT_URI, (message: IMessage) => {
+        console.log("이미지 분석 수신 : " + message.body);
         try {
           const imageResult: IImageResult = JSON.parse(message.body);
-          console.log(imageResult);
-          setSuccess(imageResult.success);
-          setEmotion(imageResult.emotion);
-          setProbability(imageResult.probability);
         } catch {
           console.log("이미지 분석 파싱 오류");
         }
       });
 
-      // 라운드 변경될 때 한 번 메시지 받음.
+      // 2. 게임 라운드가 변경되면 게임 status를 받음
       stompClient.subscribe(STATUS_URI, (message: IMessage) => {
-        console.log("게임의 상태를 수신합니다 : " + message.body);
+        console.log("게임 상태를 수신합니다.@@@@@@@@@@@ : " + message.body);
         try {
           const gameStatus: IGameStatus = JSON.parse(message.body);
+
+          // 서버로부터 "round changed" 메시지를 받을 때 round와 role을 변경
+          console.log();
+          if (gameStatus.status === "round changed") {
+            if (round === "round 1") {
+              setRound("round 2");
+              setRole((prevRole) =>
+                prevRole === "attack" ? "defense" : "attack"
+              );
+            } else if (round === "round 2") {
+              setRound("round 1");
+              setRole((prevRole) =>
+                prevRole === "attack" ? "defense" : "attack"
+              );
+            }
+          }
+
           roundEnd(gameStatus);
         } catch {
           console.log("게임 상태 파싱 오류");
@@ -148,7 +152,10 @@ export const GamePlay = () => {
       });
 
       stompClient.subscribe(RESULT_URI, (message: IMessage) => {
-        console.log("게임 결과 수신 : " + message.body);
+        console.log(
+          "게임 결과를 수신합니다.ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ : " +
+            message.body
+        );
         try {
           const gameResult: IGameResult = JSON.parse(message.body);
           gameEnd(gameResult);
@@ -157,7 +164,7 @@ export const GamePlay = () => {
         }
       });
     }
-  }, [round]);
+  }, []);
 
   // 이미지 캡처
   useEffect(() => {
@@ -166,53 +173,50 @@ export const GamePlay = () => {
         const videoElement = childRef.current?.getVideoElement();
         if (videoElement && canvasRef.current) {
           captureImage(
-            
             videoElement,
-           
             canvasRef.current,
-           
             (base64data: string) => {
-              console.log(IMAGE_DATA_URI, "로 이미지 데이터를 전송합니다.");
-              console.log("round : ", round);
-                stompClient.publish({
-                  destination: IMAGE_DATA_URI,
-                  headers: {
-                    round: round,
-                    gameSessionId: gameSessionId,
-                  },
-                  body: base64data,
-                });
-              }
-          
+              console.log(IMAGE_DATA_URI, "로 이미지를 보냅니다.");
+              stompClient.publish({
+                destination: IMAGE_DATA_URI,
+                headers: {
+                  round: round, // 여기에서 round 값을 동적으로 사용
+                  gameSessionId: gameSessionId,
+                },
+                body: base64data,
+              });
+            }
           );
         }
       } else {
         console.log("이미지 전송 실패. 연결 확인");
       }
     };
-  }, [role]);
+  }, [stompClient, round]);
 
   useEffect(() => {
-    // 웹캠 캡쳐 함수
-    clearInterval(startWebcamCapture.current);
     if (role === "defense") {
       console.log("메시지 전송 시작");
       startWebcamCapture.current = setInterval(
-        
         webcamCapture.current,
-       
         CAPTURE_INTERVAL
-      
       );
+    } else {
+      // role이 defense가 아닌 경우에는 웹캠 캡쳐 중지
+      if (startWebcamCapture.current) {
+        clearInterval(startWebcamCapture.current);
+      }
     }
   }, [role]);
+
+  const navigate = useNavigate();
 
   return (
     <div className="gameplay-page">
       <div className="gameplay-navbar">
         {/* space 균등하게 주기 위한 더미 */}
         {/* <div style={{width: 28}} /> */}
-        <Timer turn={turn} />
+        <Timer turn={role} />
         {/* 누르면 진짜 나갈건지 물어보는 모달 띄우기 / 현재 나가기가 없음 */}
         {/* <img hidden src={exitIco} alt="나가기버튼" style={{width: 28}} /> */}
       </div>

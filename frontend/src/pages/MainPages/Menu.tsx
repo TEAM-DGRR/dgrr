e10a6e30a7998a7c3fc40c6fe11c470cf9e40d56
hyuna <kook980316@gmail.com>
2023-08-13 17:37:19 +0900

    design: body height 값 변경, 메뉴 글씨 이동

--- frontend/src/pages/MainPages/Menu.tsx
+++ frontend/src/pages/MainPages/Menu.tsx
@@ -8,24 +8,27 @@ export const Menu = () => {
   const REDIRECT_LOGOUT_URI = `${process.env.REACT_APP_LOGOUT_REDIRECT_URI}`;
   const kakaoLogoutUrl = `https://kauth.kakao.com/oauth/logout?client_id=${REST_API_KEY}&logout_redirect_uri=${REDIRECT_LOGOUT_URI}`;
   const navigate = useNavigate();
-  
-const onClickBack = () => {
 
-  navigate("/main");
-}
+  const onClickBack = () => {
+    navigate("/main");
+  };
 
   return (
     <div className="MenuPage">
       <div className="MenuNav">
         <img src={backIco} onClick={onClickBack} alt="메뉴 아이콘" />
-        <span>메뉴</span>
+        <p>메뉴</p>
       </div>
       <div className="MenuBody">
         <div className="MenuBtns">
-          <img src={winkBreadIco} alt="빵_윙크" id="wink_bread"/>
+          <img src={winkBreadIco} alt="빵_윙크" id="wink_bread" />
           <p>COMMING SOON</p>
         </div>
-        <div className="MenuLogout"><span onClick={() => window.location.href = kakaoLogoutUrl}>로그아웃</span></div>
+        <div className="MenuLogout">
+          <span onClick={() => (window.location.href = kakaoLogoutUrl)}>
+            로그아웃
+          </span>
+        </div>
       </div>
     </div>
   );

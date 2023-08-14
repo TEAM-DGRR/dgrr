748bfdab8f580fe75a98193f69b690ee58f792ba
itsdangerous <danger121@naver.com>
2023-08-15 00:57:18 +0900

    test: 메인페이지에서 매칭 누르고 loading창 가기 전에 비디오 허용 여부 체크

--- frontend/src/pages/MainPages/Main.tsx
+++ frontend/src/pages/MainPages/Main.tsx
@@ -4,21 +4,30 @@ import character from "assets/images/logo_character.png";
 import title from "assets/images/logo_title.png";
 import "assets/scss/Main.scss";
 import { Button } from "components/Elements/Button/BasicButton";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
 import { ExplainModal } from "components/Elements/ExplainModal";
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Main = () => {
   const navigate = useNavigate();
   const [explainsee, setExplainsee] = useState(false);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const onClick = () => {
     // 메뉴로
     navigate("/menu");
   };
 
-  const handleRandomMatch = () => {
-    navigate("/game/loading");
+  const handleRandomMatch = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      if (stream) {
+        stream.getTracks().forEach((track) => track.stop()); // 바로 스트림을 중지
+        navigate("/game/loading");
+      }
+    } catch (err) {
+      alert("비디오 접근을 허용해주세요!"); // 사용자에게 비디오 접근 권한을 요청하는 메시지를 표시
+    }
   };
 
   // 설명 모달 보여주기
@@ -56,6 +65,7 @@ export const Main = () => {
           <Button onClick={handleRandomMatch}>랜덤 매칭</Button>
         </div>
       </div>
+      <canvas ref={canvasRef} style={{ display: "none" }}></canvas>
     </div>
   );
 };

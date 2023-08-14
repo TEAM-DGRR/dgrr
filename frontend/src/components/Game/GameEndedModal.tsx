4990e8fa3a41d940bcd53a6c5f751a2af44ecd9a
itsdangerous <danger121@naver.com>
2023-08-14 23:32:55 +0900

    design: 모달창 위로 옮김(게이지 보이게)

--- frontend/src/components/Game/GameEndedModal.tsx
+++ frontend/src/components/Game/GameEndedModal.tsx
@@ -1,11 +1,11 @@
-import "assets/scss/RoundChangeModal.scss";
+import "assets/scss/GameEndedModal.scss";
 import { memo } from "react";
 
 export const GameEndedModal = memo(
   ({ role, message }: { role: string; message: string }) => {
     return (
-      <div className="roundchange-modal-background">
-        <div className="roundchange-modal">
+      <div className="gameended-modal-background">
+        <div className="gameended-modal">
           <p>{message}</p>
           <p>@게임을 종료합니다@</p>
         </div>
